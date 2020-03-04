17076246f003ff1296dadc2a7f53adac8351c4ef
Ruben <ruben@beaxy.com>
2020-03-04 19:45:25 +0400

    Add Sendgrid email sender

---
+++ src/Viato.Api/Notification/SendGridOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Viato.Api.Notification
+{
+    public class SendGridOptions
+    {
+        [Required]
+        public string FromEmail { get; set; }
+
+        [Required]
+        public string FromName { get; set; }
+
+        [Required]
+        public string ApiKey { get; set; }
+    }
+}
---
+++ src/Viato.Api/Notification/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Viato.Api.Notification
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSendGridEmailSender(this IServiceCollection services, IConfigurationSection section)
+        {
+            var options = section.Get<SendGridOptions>();
+            services.Configure<SendGridOptions>(section);
+            services.AddTransient<ISendGridClient, SendGridClient>(x => new SendGridClient(options.ApiKey));
+            services.AddTransient<IEmailSender, SendGridEmailSender>();
+            return services;
+        }
+    }
+}
