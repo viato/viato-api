291e0a67fcf370e71e5b7a186bf4b4c7316fc6fa
arkoc <aram.koch@gmail.com>
2020-03-06 00:19:17 +0400

    Remove unused references

--- src/Viato.Api/Auth/ExternalAuthenticationGrant.cs
+++ src/Viato.Api/Auth/ExternalAuthenticationGrant.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using IdentityServer4.Models;
 using IdentityServer4.Validation;
--- src/Viato.Api/Controllers/AuthController.cs
+++ src/Viato.Api/Controllers/AuthController.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
